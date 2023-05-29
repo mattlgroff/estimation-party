5ce4f34a18653ed9460ed1cde9981abcf979949a
Matt Groff <mattlgroff@gmail.com>
2023-05-28 23:21:47 -0400

    Add Testimonials to Estimation Party

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -89,6 +89,41 @@ export default function Home() {
                         Join a room
                     </button>
                 </form>
+
+                <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+                    <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+                    <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
+                    <div className="mx-auto max-w-2xl lg:max-w-4xl">
+                        <h3 className="text-center text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl lg:text-4xl">
+                            Testimonials
+                        </h3>
+                        <hr className="mt-4 border-gray-200" />
+                        <figure className="mt-10">
+                            <blockquote className="text-center text-lg font-semibold leading-8 text-gray-900 sm:text-xl sm:leading-9">
+                                <p>
+                                    “My super talented friend and coworker, Matthew Groff, created this fun and practical tool for
+                                    estimating development work! We used Estimation Party as a team today and will be incorporating it into
+                                    our estimation meetings from now on.”
+                                </p>
+                            </blockquote>
+                            <figcaption className="mt-10">
+                                <img
+                                    className="mx-auto h-10 w-10 rounded-full"
+                                    src="https://media.licdn.com/dms/image/C5603AQHKFTl_yEb5hw/profile-displayphoto-shrink_100_100/0/1660155050233?e=1691020800&v=beta&t=KZNbc_iB1jPIcSS9lE2t7VUrikhHYgNKx4C4QSa7Vf4"
+                                    alt=""
+                                />
+                                <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                                    <div className="font-semibold text-gray-900">Meagan Rowell</div>
+                                    <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                                        <circle cx={1} cy={1} r={1} />
+                                    </svg>
+                                    <div className="text-gray-600">Product Manager @ Umbrage</div>
+                                </div>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </section>
+
                 <footer className="fixed bottom-0 flex w-full items-center justify-between bg-blue-500 px-4 py-2 text-white sm:px-6 lg:px-8">
                     <p className="text-sm">
                         &copy; {new Date().getFullYear()}
